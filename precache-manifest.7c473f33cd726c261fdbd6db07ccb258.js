949e73bf5d97174e323bf84c6180072cda5d42b3
Cynthia Deng <cynthiad215@gmail.com>
2019-02-09 12:04:25 -0800

    Updates

---
+++ precache-manifest.7c473f33cd726c261fdbd6db07ccb258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "471c238ab5c9f5e924fb",
+    "url": "/static/js/main.471c238a.chunk.js"
+  },
+  {
+    "revision": "13eeb2032981bf3107f9",
+    "url": "/static/js/1.13eeb203.chunk.js"
+  },
+  {
+    "revision": "471c238ab5c9f5e924fb",
+    "url": "/static/css/main.1fbb2237.chunk.css"
+  },
+  {
+    "revision": "4c351ab4e74145afbd8423e06981358e",
+    "url": "/index.html"
+  }
+];
