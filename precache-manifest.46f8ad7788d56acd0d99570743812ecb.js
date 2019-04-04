d0f1c4e369215fd013a25c1b3354a47efe9c4694
Cynthia Deng <cynthiad215@gmail.com>
2019-04-03 23:03:39 -0700

    Updates

---
+++ precache-manifest.46f8ad7788d56acd0d99570743812ecb.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "d20f24875705b4bf7b7638611cdfd1ee",
+    "url": "/static/media/Prototype2_homepage.d20f2487.png"
+  },
+  {
+    "revision": "9c008838906a0c3d2cf7",
+    "url": "/static/css/main.396170e1.chunk.css"
+  },
+  {
+    "revision": "bd77ea31f138ee6abaf3",
+    "url": "/static/js/1.bd77ea31.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d89b88ca89c389afd4b6277668aa36e4",
+    "url": "/static/media/profile_s.d89b88ca.jpg"
+  },
+  {
+    "revision": "792f17d77897d58e55ab65bbd68fb281",
+    "url": "/static/media/ux_field.792f17d7.jpg"
+  },
+  {
+    "revision": "5325f7dca0d1a9cc0b7d7427cea31dfb",
+    "url": "/static/media/prototype1_main.5325f7dc.png"
+  },
+  {
+    "revision": "9c008838906a0c3d2cf7",
+    "url": "/static/js/main.9c008838.chunk.js"
+  },
+  {
+    "revision": "0ec764d192028886943e6fc1ce1d4ab3",
+    "url": "/static/media/UXLab_Horizontal_Blue.0ec764d1.png"
+  },
+  {
+    "revision": "ccf31fe51b5877bf9ec5ba243a02c959",
+    "url": "/static/media/uxanalysis.ccf31fe5.jpg"
+  },
+  {
+    "revision": "3164789e06bd128ab898a29b328f538a",
+    "url": "/static/media/research-plan.3164789e.jpg"
+  },
+  {
+    "revision": "91b616f1d9022e53d116343eadbf531a",
+    "url": "/static/media/lumohacks-homepage.91b616f1.png"
+  },
+  {
+    "revision": "cbfda5e8fef51cae58c270993ae1b8bb",
+    "url": "/static/media/raceforce-logo.cbfda5e8.png"
+  },
+  {
+    "revision": "3336510941bbad101cb5c27f62344fd7",
+    "url": "/index.html"
+  }
+];
